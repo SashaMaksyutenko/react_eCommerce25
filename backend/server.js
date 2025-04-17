10a7e16c339c4b24717da311b98df1ccdb1453e1
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-04-17 19:27:36 +0300

    users routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-const express=require('express')
-const cors=require('cors')
-const app=express()
-const dotenv=require('dotenv')
-const connectDB=require('./config/db')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 app.use(express.json())
 app.use(cors())
 dotenv.config()
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 // connection to MongoDB
 connectDB()
-app.get('/',(req,res)=>{
-    res.send('welcome to RABBIT API')
+app.get('/', (req, res) => {
+  res.send('welcome to RABBIT API')
 })
-app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`)
+// API routes
+app.use('/api/users', userRoutes)
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`)
 })
